0de41db152e2a635918b9a78f851286e6e28718a
aljaz <aljazpun@gmail.com>
2024-09-19 16:50:10 +0200

    init commit #2

--- packages/strava/webhook/index.js
+++ packages/strava/webhook/index.js
@@ -1,1 +1,19 @@
-export async function main(_, context) {}
+export function main(event, context) {
+  // Handle subscription verification
+  if (
+    event["hub.mode"] === "subscribe" &&
+    event["hub.verify_token"] === process.env.STRAVA_VERIFY_TOKEN &&
+    event["hub.challenge"]
+  ) {
+    return {
+      statusCode: 200,
+      body: { "hub.challenge": event["hub.challenge"] },
+    };
+  }
+
+  return {
+    statusCode: 200,
+  };
+}
+
+function getAccessToken() {}
